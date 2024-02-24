f3958c8a6896eea932bdeafd789c9a124592dddc
George Mark <georgemarkokumu@gmail.com>
2024-02-24 20:05:45 +0300

    Create a python file

---
+++ user_input.py
@@ -0,0 +1,4 @@
+name = input("Enter your name ")
+age = int(input("Enter your age "))
+location = input("Enter your location ")
+print("Hello", name, "you are", age, "years old and live in", location)
